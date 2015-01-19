bc4b79c47e5cce1c519d1563b97c38266f944313
okmttdhr <10101114fin@gmail.com>
2015-01-19 18:06:44 +0900

    step-10: localStorageServiceで保存。テストがうまくいかない

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -5,29 +5,56 @@ describe('Controller: MainCtrl', function () {
   // load the controller's module
   beforeEach(module('mytodoApp'));
 
-  var MainCtrl,
-    scope;
+  var MainCtrl;
+  var scope;
+  var localStorageService;
+  var store;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, _localStorageService_) {
+    store = []; //clear the store before each test
     scope = $rootScope.$new();
+    localStorageService = _localStorageService_;
+
+    //mock localStorageService get/add
+    spyOn(localStorageService,'get').and.callThrough();
+    // callFake(function(key){
+    //   return store[key];
+    // });
+    spyOn(localStorageService,'add').and.callThrough();
+    // callFake(function(key, val){
+    //   store[key] = val;
+    // });
+
     MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+      $scope: scope,
+      localStorageService: _localStorageService_
     });
+
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
+  it('should attach a list of todos to the scope', function () {
     expect(scope.todos.length).toBe(0);
   });
 
   it('should add items to the list', function () {
-    scope.todo = 'Test 1';
+    scope.todo = {
+      title: 'Test 1'
+    };
     scope.addTodo();
     expect(scope.todos.length).toBe(1);
+
+    expect(localStorageService.get).toHaveBeenCalledWith('todos');
+    // expect(localStorageService.get).toHaveBeenCalled('todos');
+    // expect(localStorageService.add).toHaveBeenCalledWith('todos', jasmine.any(String));
+    expect(localStorageService.get('todos')).toEqual(scope.todos);
+
   });
 
   it('should add then remove an item from the list', function () {
-    scope.todo = 'Test 1';
+    scope.todo = {
+      title: 'Test 1'
+    };
     scope.addTodo();
     scope.removeTodo(0);
     expect(scope.todos.length).toBe(0);
